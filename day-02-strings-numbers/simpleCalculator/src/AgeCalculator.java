0e4815fe014315ba4b21a688043565cbb18fea21
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-05 13:41:39 +0200

    updated the agecalculator

--- day-02-strings-numbers/simpleCalculator/src/AgeCalculator.java
+++ day-02-strings-numbers/simpleCalculator/src/AgeCalculator.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class AgeCalculator {
     public static void main(String[] args){
@@ -7,7 +9,35 @@ public class AgeCalculator {
 
 
         while (choice.equalsIgnoreCase("yes")) {
-            System.out.print("hello");
+            System.out.println("===== Welcome to my Mini Age Calculator👯‍♀️ =====");
+
+
+            //GETTING THE USERSS Name
+            System.out.println("Enter your Name: ");
+            String name = input.nextLine();
+
+            //BIRTHDAY DETAILS
+            System.out.print("Enter your birthday year: ");
+            int birthYear = input.nextInt();
+
+            System.out.print("Enter your birth month (1-12): ");
+            int birthMonth = input.nextInt();
+
+            System.out.print("Enter your Birth date(1-31): ");
+            int date = input.nextInt();
+
+            //CREATING THE DATE OBJECT & current date
+            LocalDate birthDate;
+            try{
+                birthDate = LocalDate.of(birthYear, birthMonth, date);
+            } catch (Exception e){
+                System.out.println("⚠️ Hmm... that date looks off. Try again maybe?");
+                return;
+            }
+
+            //CALCULATING THE AGE
+            
+
 
 
             System.out.print("Do you want to calculate Again?: ");
