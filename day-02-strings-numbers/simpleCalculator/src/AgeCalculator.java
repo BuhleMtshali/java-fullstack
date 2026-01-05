db7addf2e7cf4f99d388039aa75792848e419e9f
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-05 13:08:17 +0200

    created the while loop in the age calculator

--- day-02-strings-numbers/simpleCalculator/src/AgeCalculator.java
+++ day-02-strings-numbers/simpleCalculator/src/AgeCalculator.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class AgeCalculator {
     public static void main(String[] args){
-        System.out.print("hello");
+        Scanner input = new Scanner(System.in);
+        String choice = "yes";
+
+
+        while (choice.equalsIgnoreCase("yes")) {
+            System.out.print("hello");
+
+
+            System.out.print("Do you want to calculate Again?: ");
+            choice = input.nextLine();
+            
+        }
+
+
+        
+        System.out.println("Thank you trying my Mini Age Calculator!!!");
+        input.close();
     }
 }
