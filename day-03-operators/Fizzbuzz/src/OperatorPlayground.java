fdd5d31f6615553a17e6b3e4417512248b2235f6
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-06 13:51:44 +0200

    updated the operator options

--- day-03-operators/Fizzbuzz/src/OperatorPlayground.java
+++ day-03-operators/Fizzbuzz/src/OperatorPlayground.java
@@ -4,11 +4,16 @@ public class OperatorPlayground {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         String runAgain = "yes";
+        int option;
 
         //STARTING THE WHILE LOOP
         while (runAgain.equalsIgnoreCase("yes")) {
-            System.out.print("hello");
-
+            System.out.println("===== Operator Playground 🎮 =====");
+            System.out.println("1. Arithmetic Operations🧮");
+            System.out.println("2. Comparison Check👾");
+            System.out.println("3. Logical Decision Test🧩");
+            System.out.println("4. Exit🚫");
+            System.out.print("Choose an option from above: ");
 
             //ASKING IF WE WANT TO RERUN THE LOOP OR NOT
             System.out.print("Woud you like to Rerun the program?(yes/no): ");
