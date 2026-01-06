3c92ea362095257c6bdd5cfc824c0550d24ee6f2
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-06 14:55:16 +0200

    updated the operator

--- day-03-operators/Fizzbuzz/src/OperatorPlayground.java
+++ day-03-operators/Fizzbuzz/src/OperatorPlayground.java
@@ -14,6 +14,24 @@ public class OperatorPlayground {
             System.out.println("3. Logical Decision Test🧩");
             System.out.println("4. Exit🚫");
             System.out.print("Choose an option from above: ");
+            option = input.nextInt();
+
+            //STARTING THE SWITCH LOOP
+            switch (option) {
+                case 1:
+                    System.out.println("=== Arithmetic Operations🧮 ====");
+                    System.out.print("Enter the first number: ");
+                    Double firstNumber = input.nextDouble();
+                    System.out.print("Choose an operator(+, -, *, /): ");
+                    char operator = input.next().charAt(0);
+                    System.out.print("Enter your second number: ");
+                    Double secondNumber = input.nextDouble();
+                    
+                    break;
+            
+                default:
+                    break;
+            }
 
             //ASKING IF WE WANT TO RERUN THE LOOP OR NOT
             System.out.print("Woud you like to Rerun the program?(yes/no): ");
