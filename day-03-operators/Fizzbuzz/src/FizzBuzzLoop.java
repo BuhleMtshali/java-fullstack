07ae15a2eb543edd18b4ff8325901677339f348f
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-06 13:22:18 +0200

    updated the fizzbuzz

--- day-03-operators/Fizzbuzz/src/FizzBuzzLoop.java
+++ day-03-operators/Fizzbuzz/src/FizzBuzzLoop.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class FizzBuzzLoop {
     public static void main(String[] args){
-        System.out.print("hello");
+       Scanner input = new Scanner(System.in);
+
+       String runAgain = "yes";
+
+       System.out.println("====🔐 FizzBuzz Mini Game🧩 =====");
+    
+       //START OF THE LOOP
+       while (runAgain.equalsIgnoreCase("yes")) {
+        System.out.print("loop running");
+
+        System.out.print("Enter your Max Iterator(1-30): ");
+        int maxIterator = input.nextInt();
+
+        for(int i = 0; i <= maxIterator; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzzBuzzz!!");
+            } else if(i % 3 == 0){
+                System.out.println("Fizzzz!!");
+            } else if(i % 5 == 0){
+                System.out.println("Buzz!!!");
+            } else{
+                System.out.println(i);
+            }
+        }
+
+
+
+        //ASKING TO SEE IF THE LOOP RERUNS OR NOT
+        System.out.print("Do you want to rerun the loop?(yes/no): ");
+        input.nextLine();
+        runAgain = input.nextLine();
+
+       }
+
+       System.out.print("Loop has ended!!");
+       input.close();
     }
 }
