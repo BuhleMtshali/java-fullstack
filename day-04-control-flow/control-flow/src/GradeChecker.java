b71ede65bf76e2a174ba09f63b80651bef194f57
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-09 10:18:45 +0200

    UPDATED the grade checker

--- day-04-control-flow/control-flow/src/GradeChecker.java
+++ day-04-control-flow/control-flow/src/GradeChecker.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class GradeChecker {
     public static void main(String[] args){
-        System.out.print("hello");
+        Scanner input = new Scanner(System.in);
+        System.out.println("====== 🎲Mini Grade Checker 🐝 ======");
+        String playAgain = "yes";
+        //STARTING LOOP
+        while (playAgain.equalsIgnoreCase("yes")) {
+            System.out.print("Loop is running....");
+
+
+            //CLOSING THE LOOP
+            System.out.println("🎰 Do you want to Calculate Again(yes/no): ");
+            playAgain = input.nextLine();
+        }
+
+
+        System.out.println("===== 🧩 THANK YOU FOR TRYING MY GRADE CHECKER 🐝 ======");
+        //CLOSING THE INPUT
+        input.close();
     }
 }
