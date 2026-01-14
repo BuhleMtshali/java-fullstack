8151e453c8f2a1cfec0de4a01040b34b958f9699
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-14 19:49:00 +0200

    updated the print statement

--- day-06-arrays/StudentGrader.java
+++ day-06-arrays/StudentGrader.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 public class StudentGrader {
     public static void main(String[] args) {
         ArrayList<Double> grades = new ArrayList<>();
-        
+        System.out.print("Leaning more about arrays");
     }
 }
