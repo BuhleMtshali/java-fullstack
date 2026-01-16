11adcc5fff77de0e6a956e793032b704c06186ec
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-16 19:04:15 +0200

    refactor: enhance student grader functionality to allow multiple grade entries

--- day-06-arrays/StudentGrader.java
+++ day-06-arrays/StudentGrader.java
@@ -1,9 +1,25 @@
 //IMPORTING THE JAVA UTIL ARRAYLIST
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StudentGrader {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         ArrayList<Double> grades = new ArrayList<>();
-        System.out.print("Leaning more about arrays");
+        System.out.println("====== 🎲 Grade List 📋 =====");
+
+        String addMore = "yes";
+        while (addMore.equalsIgnoreCase("yes")) {
+            
+
+            //CLOSING THE LOOP
+            System.out.print("🎰 Wanna add more grades? ");
+            input.nextLine();
+            addMore = input.nextLine();
+        }
+
+
+        System.out.print("======= 🐝 oHKAY List is in 🎮 ======");
+        input.close();
     }
 }
