3c720becebe89f5bad635e3b7a1c77f553b2ac1a
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-16 21:15:28 +0200

    refactor: improve code readability by adding comments and formatting in StudentGrader

--- day-06-arrays/StudentGrader.java
+++ day-06-arrays/StudentGrader.java
@@ -8,18 +8,29 @@ public class StudentGrader {
         ArrayList<Double> grades = new ArrayList<>();
         System.out.println("====== 🎲 Grade List 📋 =====");
 
+        //STARTING A WHILE LOOP FOR USER TO ADD MORE GRADES
+
         String addMore = "yes";
         while (addMore.equalsIgnoreCase("yes")) {
-            
-
+            System.out.print("🧩 Enter a grade: ");
+            double grade = input.nextDouble();
+            grades.add(grade);
             //CLOSING THE LOOP
             System.out.print("🎰 Wanna add more grades? ");
             input.nextLine();
             addMore = input.nextLine();
         }
+        //END OF THE LOOP for adding MORE GRADES
 
 
         System.out.print("======= 🐝 oHKAY List is in 🎮 ======");
+        
+        //LOOPING THROUGH THE ARRAY
+        for(double grade : grades){
+            System.out.println(grade);
+        }
+
+        
         input.close();
     }
 }
