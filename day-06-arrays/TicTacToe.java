9f8c3c4e6cbd971c64982c1536393f1c949361f6
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-18 17:55:25 +0200

    feat: initialize game board in TicTacToe class

--- day-06-arrays/TicTacToe.java
+++ day-06-arrays/TicTacToe.java
@@ -1,5 +1,10 @@
 public class TicTacToe {
     public static void main(String[] args){
-        
+        //FIRST WE CREATE CHAR[][] WHICH WILL HOLD 1 CHARACTER EACH
+        char [][] board = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+        };
     }
 }
