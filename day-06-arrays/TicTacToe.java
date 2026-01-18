7905a8f65f2e872f32a658208357797dddc74db0
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-18 18:05:29 +0200

    feat: implement board printing using loops in TicTacToe class

--- day-06-arrays/TicTacToe.java
+++ day-06-arrays/TicTacToe.java
@@ -17,5 +17,23 @@ public class TicTacToe {
             board[0][0] + " | " + board[0][1] + " | " + board[0][2]
         );
 
+        //PRINTING THE BOARD USING LOOPS
+        //1. OUTER LOOP
+        for(int row = 0; row < board.length; row++){
+            //2. INNER LOOP
+            for(int col = 0; col < board[row].length; col++){
+                System.out.print(board[row][col]);
+
+                if(col < 2){
+                    System.out.print(" | ");
+                }
+            }
+
+            System.out.println();
+            if(row < 2){
+                System.out.println("---+---+---");
+            }
+        }
+
     }
 }
