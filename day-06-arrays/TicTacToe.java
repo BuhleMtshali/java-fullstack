362fd3cb0eb7a1d6ef2d44cada62524b23b91be6
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-18 18:06:57 +0200

    refactor: simplify board initialization and printing in TicTacToe class

--- day-06-arrays/TicTacToe.java
+++ day-06-arrays/TicTacToe.java
@@ -1,39 +1,25 @@
 public class TicTacToe {
-    public static void main(String[] args){
-        //FIRST WE CREATE CHAR[][] WHICH WILL HOLD 1 CHARACTER EACH
-        char [][] board = {
-            {' ', ' ', ' '},
-            {' ', ' ', ' '},
-            {' ', ' ', ' '}
+    public static void main(String[] args) {
+
+        char[][] board = {
+            {'X', 'O', 'X'},
+            {'O', 'X', ' '},
+            {' ', 'O', 'X'}
         };
 
-        //indexes start at 0
-        //board[0][0] // top-left
-        //board[1][1] // center
-        // board[2][2] // bottom-right
+        printBoard(board);
+    }
 
-        //PRINTING the first ROW manually
-        System.out.println(
-            board[0][0] + " | " + board[0][1] + " | " + board[0][2]
-        );
+    public static void printBoard(char[][] board) {
+        for (int row = 0; row < board.length; row++) {
 
-        //PRINTING THE BOARD USING LOOPS
-        //1. OUTER LOOP
-        for(int row = 0; row < board.length; row++){
-            //2. INNER LOOP
-            for(int col = 0; col < board[row].length; col++){
+            for (int col = 0; col < board[row].length; col++) {
                 System.out.print(board[row][col]);
-
-                if(col < 2){
-                    System.out.print(" | ");
-                }
+                if (col < 2) System.out.print(" | ");
             }
 
             System.out.println();
-            if(row < 2){
-                System.out.println("---+---+---");
-            }
+            if (row < 2) System.out.println("---+---+---");
         }
-
     }
 }
