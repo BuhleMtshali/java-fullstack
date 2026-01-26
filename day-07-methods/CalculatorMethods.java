f05a3765a4d25d8ea65dc34f6a0430f00de5ad06
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-26 11:35:21 +0200

    created the class

--- day-07-methods/CalculatorMethods.java
+++ day-07-methods/CalculatorMethods.java
@@ -1,11 +1,16 @@
-class CalculatingMethods{
-    
-    
-}
-
-
-
 public class CalculatorMethods {
+        public class Calculator {
+
+        public int addNumbers(int a, int b){
+            int sum = a + b;
+            return sum;
+        }
+    }
+
+
+
+
+
     public static void main(String[] args){
         System.out.print("hello");
     }
