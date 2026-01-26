a93efe9246cef9f5397dbbf69d3dbf838b07ee8a
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-26 18:03:00 +0200

    created the basic methods

--- day-07-methods/CalculatorMethods.java
+++ day-07-methods/CalculatorMethods.java
@@ -1,16 +1,18 @@
-public class CalculatorMethods {
-        public class Calculator {
-
-        public int addNumbers(int a, int b){
-            int sum = a + b;
-            return sum;
-        }
+class Calculator{
+    public static double add(double num1, double num2){
+        double sum = num1 + num2;
+        return sum;
     }
 
+    public static double minus(double num1, double num2){
+        double sum = num1 - num2;
+        return sum;
+    }
+}
 
 
 
-
+public class CalculatorMethods {
     public static void main(String[] args){
         System.out.print("hello");
     }
