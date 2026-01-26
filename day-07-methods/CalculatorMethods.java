7e07fc737088c484950f6aebbaec9fc596fa5ea8
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-26 18:11:29 +0200

    updated the calculator methods

--- day-07-methods/CalculatorMethods.java
+++ day-07-methods/CalculatorMethods.java
@@ -8,6 +8,19 @@ class Calculator{
         double sum = num1 - num2;
         return sum;
     }
+
+    public static double multiplay(double num1, double num2){
+        double sum = num1 * num2;
+        return sum;
+    }
+
+    public static double divide(double num1, double num2){
+        if(num2 == 0){
+            System.out.println("‼️ We cannot divide by 0");
+            return 0;
+        }
+        return num1 / num2;
+    }
 }
 
 
